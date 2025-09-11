7c54664eaca3e0efaaaaa4d20967e70db6461482
Squikys <kaustavmahata@gmail.com>
2025-09-11 22:05:52 +0530

    add send email logic

---
+++ services/email_client.py
@@ -0,0 +1,37 @@
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+import logging
+logging.basicConfig(format='%(asctime)s %(message)s')
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
+def send_mail(mail:str,passwd:str,receiver:list[str]):
+
+    smtp_server = "smtp.gmail.com"
+    smtp_port = 587
+    username = mail
+    password = passwd
+
+    sender_email = mail
+    receiver_email = receiver
+    subject = "Test Email from Python"
+    body = "Hello! This is a test email sent from Python."
+
+    msg = MIMEMultipart()
+    msg['From'] = sender_email
+    msg['To'] = receiver_email
+    msg['Subject'] = subject
+
+    msg.attach(MIMEText(body, 'plain'))
+
+    try:
+        server = smtplib.SMTP(smtp_server, smtp_port)
+        server.starttls()  # Secure the connection
+        server.login(username, password)
+        server.sendmail(sender_email, receiver_email, msg.as_string())
+        logging.info("Email sent successfully!")
+    except Exception as e:
+        logging.error(f"Error sending email: {e}")
+    finally:
+        server.quit()
