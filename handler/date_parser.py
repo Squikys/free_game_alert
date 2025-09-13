e3c2192a22461d8b6aa5f8239328025bc5662b60
Squikys <kaustavmahata@gmail.com>
2025-09-13 17:07:48 +0530

    added date formatter that converts date to more readable with ist time

--- handler/date_parser.py
+++ handler/date_parser.py
@@ -7,3 +7,12 @@ def date_validator(time_stamp:str,limit_hours:float)->bool:
     within_24h = now - timestamp <= timedelta(hours=limit_hours)
 
     return within_24h
+
+def formatted_time(iso_date:str)->str:
+
+    import pytz
+    dt = datetime.fromisoformat(iso_date)
+    ist = pytz.timezone("Asia/Kolkata")
+    dt_ist = dt.astimezone(ist)
+    formatted_date = dt_ist.strftime("%B %d, %Y %I:%M %p")
+    return formatted_date
